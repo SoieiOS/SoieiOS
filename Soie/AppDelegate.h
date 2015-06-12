3b05f19154145b3caf26467906ed2ca772636969
abhishek4466 <abhishek.tyagi@quovantis.com>
2015-06-12 20:24:49 +0530

    Initial Commit

---
+++ Soie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Soie
+//
+//  Created by Abhishek Tyagi on 12/06/15.
+//  Copyright (c) 2015 Abhishek Tyagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Soie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Soie
+//
+//  Created by Abhishek Tyagi on 12/06/15.
+//  Copyright (c) 2015 Abhishek Tyagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
